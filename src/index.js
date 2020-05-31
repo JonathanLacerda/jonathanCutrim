a62be950a8b9c2ef05185a484eddea8f8a1135de
jlacerda <jlacerda@itelios.com>
2020-05-31 15:38:59 -0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*
+|--------------------------------------------------------------------------
+| Main Component - App
+|--------------------------------------------------------------------------
+*/
+
+// Import Style/Scripts
+
+import './sass/style.scss';
+import initApp from './js/core/core'
+
+const app = initApp();
+
+try{
+    app.init()
+
+} catch(error){
+    console.log(error)
+}
---
+++ src/js/core/core.js
@@ -0,0 +1,22 @@
+/*
+|--------------------------------------------------------------------------
+| App
+|--------------------------------------------------------------------------
+*/
+
+// Import objects
+
+const anime = require('animejs');
+
+function core(){
+
+    function init(){
+
+    }
+
+    return {
+        init
+    }
+}
+
+export default core
