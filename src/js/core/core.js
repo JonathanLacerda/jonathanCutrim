74acdf0835c979e1cc79ab91ad7be997d85a3e8a
jlacerda <jlacerda@itelios.com>
2020-06-09 16:10:01 -0300

    feat: New library imagesLoaded, and smooth scroll

--- src/js/core/core.js
+++ src/js/core/core.js
@@ -7,11 +7,12 @@
 // Import objects
 
 const anime = require('animejs');
+import smoothScroll from '../modules/smoothScroll'
 
 function core(){
 
     function init(){
-
+        smoothScroll()
     }
 
     return {
